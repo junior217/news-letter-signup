1fdc7b51e100e4a8f76540bde9e205ce3eea8bd2
John J Eze <johneze.webdev03@gmail.com>
2025-05-22 12:57:10 +0000

    commented route.ts in verify

--- news-letter/app/api/verify/route.ts
+++ news-letter/app/api/verify/route.ts
@@ -1,31 +1,31 @@
-import { emaillist} from "@/app/data";
-import { NextRequest } from "next/server";
+// import { emaillist} from "@/app/data";
+// import { NextRequest } from "next/server";
 
 
-type EmailList = Record<string, string>
+// type EmailList = Record<string, string>
 
-const emails: EmailList = {
-    '1': 'john@gmail.com'
-}
+// const emails: EmailList = {
+//     '1': 'john@gmail.com'
+// }
 
-type Params = {
-    id: string;
-}
+// type Params = {
+//     id: string;
+// }
 
-export async function GET() {
+// export async function GET() {
 
-  const emails = emaillist
-  return new Response(JSON.stringify(emails), {
-    status: 200,
-  });
-}
+//   const emails = emaillist
+//   return new Response(JSON.stringify(emails), {
+//     status: 200,
+//   });
+// }
 
 
-export async function POST(request: NextRequest, { params }: { params: Params }) {
-  const userId = params.id;
-  const body: EmailList = await request.json();
-  const emailId = body.userId;
+// export async function POST(request: NextRequest, { params }: { params: Params }) {
+//   const userId = params.id;
+//   const body: EmailList = await request.json();
+//   const emailId = body.userId;
 
-//   emails[userId] = emails[userId] ? emails[userId].concat(emailId) : [emailId];
-//   const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
-}
+// //   emails[userId] = emails[userId] ? emails[userId].concat(emailId) : [emailId];
+// //   const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+// }
